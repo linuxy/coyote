7021c8a6f9c51440ab5f21077b7e7883099fe440
Ian Applegate <ian@jedi.sh>
2022-05-08 21:19:15 -0400

    echo example

---
+++ examples/coyote_echo.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const log = std.log.scoped(.echo);
+const Coyote = @import("coyote");
+
+pub fn main() !void {
+    var coyote = try Coyote.init();
+    defer coyote.deinit();
+    try coyote.config(.{.listen = "localhost",
+                        .port = 8080});
+    try coyote.run();
+}
+
+pub const coyote_index = struct {
+    const Self = @This();
+
+    route: []const u8 = "/echo",
+    handler: fn(req: Coyote.Request, data: Coyote.Data) u32 = handler,
+    flags: u32 = Coyote.All,
+
+    pub fn handler(req: Coyote.Request, data: Coyote.Data) u32 {
+        try Coyote.response(req, 200, "text/plain", std.mem.span(req.*.body), data);
+        return Coyote.Processed;
+    }
+};
